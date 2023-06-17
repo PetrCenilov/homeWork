8d7c265670728a73f6b25643bef69d2b326f93bf
Petr <pilot_master_68@mail.ru>
2023-06-17 12:38:37 +0300

    21.8 dobavlenie top bar,tool bar,tosti,

--- app/src/main/java/com/example/poiskfilmoff/MainActivity.kt
+++ app/src/main/java/com/example/poiskfilmoff/MainActivity.kt
@@ -2,11 +2,48 @@ package com.example.poiskfilmoff
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.poiskfilmoff.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setOnMenuItemClickListener () {
+            when (it.itemId) {
+                R.id.settings-> {
+                    Toast.makeText(this,"Настройки",Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else->false
+            }
+        }
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.favorites -> {
+                    Toast.makeText(this, "Избранное", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.watch_later -> {
+                    Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.selections -> {
+                    Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+
+        }
+
 
     }
 
